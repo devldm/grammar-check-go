36e0872c8436a64707ba76c8f86db697e0b34629
liam <39243060+devldm@users.noreply.github.com>
2024-01-27 18:37:11 +0900

    add challenges and profile page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,37 +1,14 @@
-import { grammar } from "@/types/grammar";
-
-async function getData() {
-  const res = await fetch(`${process.env.API_BASE_URL}/grammars`);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
+import Link from "next/link";
 
 export default async function Home() {
-  const data = await getData();
-  console.log(data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 dark:text-white">
-      <div className="flex flex-col gap-4 w-full items-center">
-        {data.map((item: grammar) => {
-          return (
-            <div
-              key={item.ID}
-              className="border-2 border-black dark:border-white rounded-lg p-6 w-full md:w-[60%]"
-            >
-              <h1 className="text-2xl">{item.Grammar}</h1>
-              <p className="dark:text-[#e6e6e6]">{item.Description.String}</p>
-              <br></br>
-              <div className="text-sm border-2 border-orange-500 max-w-max p-2 rounded-full">
-                <p>{item.Difficulty.String}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+    <main className="flex min-h-screen flex-col items-center p-12 dark:text-white">
+      <Link className="text-2xl text-blue-500 font-bold" href={"/challenges"}>
+        Challenges
+      </Link>
+      <Link className="text-2xl text-blue-500 font-bold" href={"/profile"}>
+        Profile
+      </Link>
     </main>
   );
 }
