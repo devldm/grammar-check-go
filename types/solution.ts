f211c6bfcd38978a3204cf789980616b1e3dade8
liam <39243060+devldm@users.noreply.github.com>
2024-02-15 16:29:36 +0900

    improve types for users and solutions

--- types/solution.ts
+++ types/solution.ts
@@ -13,3 +13,17 @@ export type solutionPost = {
   grammar_id: string;
   user_id: string;
 };
+
+export type solutionWithUserData = {
+  ID: string;
+  CreatedAt: string;
+  UpdatedAt: string;
+  GrammarID: string;
+  UserID: string;
+  Solution: string;
+  Grammar: string;
+  ClerkUsername: string;
+  ClerkEmail: string;
+  ClerkImage: string;
+  UserID_2: string;
+};
---
+++ types/user.ts
@@ -0,0 +1,8 @@
+type backendUser = {
+  ID: string;
+  CreatedAt: string;
+  UpdatedAt: string;
+  ClerkUsername: string;
+  ClerkEmail: string;
+  ClerkImage: string;
+};
