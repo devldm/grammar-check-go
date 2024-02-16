df061be0ce0f1021dee5974555fd21d6cd822aa0
liam <39243060+devldm@users.noreply.github.com>
2024-02-16 12:39:10 +0900

    add basic error page

---
+++ app/(pages)/error.tsx
@@ -0,0 +1,24 @@
+"use client"; // Error components must be Client Components
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="min-h-[80vh] flex flex-col items-center justify-center w-full">
+      <h2 className="text-4xl mb-5">Something went wrong!</h2>
+      <button
+        className="btn btn-secondary rounded-md"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
