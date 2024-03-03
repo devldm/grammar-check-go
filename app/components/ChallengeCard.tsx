2c20fdd44e59b2ca8773f494f7fdde88e8f87ed5
liam <39243060+devldm@users.noreply.github.com>
2024-03-03 17:39:16 +0900

    updates to challenge card

--- app/components/ChallengeCard.tsx
+++ app/components/ChallengeCard.tsx
@@ -3,13 +3,15 @@ import Spacer from "./Spacer";
 
 export default function ChallengeCard({ item }: { item: grammar }) {
   return (
-    <div className="border-2 border-white rounded-lg p-6 max-w-md">
-      <h1 className="text-lg font-bold md:text-2xl">{item.Grammar}</h1>
-      <p className="dark:text-[#e6e6e6]">{item.Description.String}</p>
-      <Spacer height="h-2 md:h-4" />
-      <div className="text-sm text-orange-500 bg-gray-700 max-w-max px-2 py-1 rounded-full">
-        <p className="font-medium">{item.Difficulty.String}</p>
+    <div className="border-2 border-white rounded-lg p-6 max-w-md h-full flex justify-between flex-col">
+      <div>
+        <h1 className="text-lg font-bold md:text-2xl">{item.Grammar}</h1>
+        <p className="dark:text-[#e6e6e6]">{item.Description.String}</p>
       </div>
-    </div>
+      <Spacer height="h-2 md:h-4" />
+      <div className="self-end">
+        <p className="max-w-max rounded-full text-sm px-2 py-1 text-orange-500 bg-gray-700 font-medium">{item.Difficulty.String}</p>
+      </div>
+    </div >
   );
 }
